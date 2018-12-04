743b811cc603b55355e5eb4799c5d662ffd31563
Johnny Sins <jepf_27@hotmail.es>
2018-12-04 16:11:46 -0600

    actualice

--- prujam.c
+++ prujam.c
@@ -3,10 +3,10 @@
 
 int main(int argc, char const *argv[]){
 
-	printf("hola mundo\n");
-	printf("jajaja xd\n");
-	printf(" >:v \n");
-	printf("8=====D\n");
+	printf("hola vida\n");
+	printf("jajaja xdxd\n");
+	printf(" >:vV \n");
+	printf("8================D\n");
 	
 	return 0;
 }
