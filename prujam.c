1810eb2ea0c9c2876a6927e88b34e28a1c1bedde
alejandro pacheco <pachecofrancoalejandro@gmail.com>
2018-12-04 15:38:16 -0600

    no se jajaxd

---
+++ prujam.c
@@ -0,0 +1,12 @@
+//prueba jamon
+#include <stdio.h>
+
+int main(int argc, char const *argv[]){
+
+	printf("hola mundo\n");
+	printf("jajaja xd\n");
+	printf(" >:v \n");
+	printf("8=====D\n");
+	
+	return 0;
+}
